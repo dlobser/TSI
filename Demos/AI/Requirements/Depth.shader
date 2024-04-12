987443a006d3fd5ab0c21c035f425c95d6655967
David Lobser <davidlobser@yahoo.com>
2024-04-12 11:12:49 -0400

    added AI demo

---
+++ Demos/AI/Requirements/Depth.shader
@@ -0,0 +1,81 @@
+﻿Shader "Unlit/Depth"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _DepthOffset ("Depth Offset", float) = 0 // This replaces _Data.x
+        _DepthMultiplier ("Depth Multiplier", float) = 0 // This replaces _Data.y
+        _Adjustment ("Adjustment", float) = 0 // This replaces _Data.w
+        _CamPos("CamPos", vector) = (0,0,0,0)
+        _Pow ("Pow", float) = 0
+        _NoiseAmount ("Noise Amount", float) = 0
+        _NoiseFrequency ("Noise Frequency", float) = 1
+        _NoiseSpeed ("Noise Speed", float) = 0 
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+            #include "Noise.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                float3 pos : COLOR;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                UNITY_FOG_COORDS(1)
+                float4 vertex : SV_POSITION;
+                float3 pos : COLOR;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            float4 _CamPos;
+            float _Pow;
+            float _NoiseAmount;
+            float _NoiseFrequency;
+            float _NoiseSpeed;
+
+            // Added individual variables
+            float _DepthOffset;
+            float _DepthMultiplier;
+            float _Adjustment;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                o.pos = mul(unity_ObjectToWorld, v.vertex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                float noise = snoise(i.pos*_NoiseFrequency+_NoiseSpeed*_Time.y);
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return lerp(1, (noise+1)/2, _NoiseAmount) * pow((length(i.pos-_WorldSpaceCameraPos)+_DepthOffset)*_DepthMultiplier, _Pow) + _Adjustment;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Demos/AI/Requirements/worldUVs.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class worldUVs : MonoBehaviour
+{
+    public Transform target;
+    public string path = "Assets";
+    public string name = "BakedUVMesh";
+    Camera cam;
+    public bool saveMeshes;
+    public bool mergeMeshes = true; // Flag to control whether to merge meshes or not
+
+    List<CombineInstance> combineInstances = new List<CombineInstance>();
+
+    void Start()
+    {
+        cam = Camera.main;
+        cam.aspect = 1;
+        Recurse(target, 0);
+
+        if (mergeMeshes)
+        {
+            CombineAllMeshes();
+        }
+    }
+
+    void Recurse(Transform t, int overflow)
+    {
+        overflow++;
+        if (overflow < 1e5)
+        {
+            MeshFilter meshFilter = t.GetComponent<MeshFilter>();
+            if (meshFilter != null)
+            {
+                ProjectUV(meshFilter);
+                if (mergeMeshes)
+                {
+                    AddMeshToCombine(meshFilter);
+                }
+                else
+                {
+                    SaveIndividualMesh(meshFilter);
+                }
+            }
+            for (int i = 0; i < t.childCount; i++)
+            {
+                Recurse(t.GetChild(i), overflow);
+            }
+        }
+    }
+
+    void ProjectUV(MeshFilter meshFilter)
+    {
+        Mesh mesh = meshFilter.mesh;
+        Vector3[] vertices = mesh.vertices;
+        Vector2[] uvs = new Vector2[vertices.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 worldPoint = meshFilter.transform.TransformPoint(vertices[i]);
+            Vector3 screenPos = cam.WorldToScreenPoint(worldPoint);
+            uvs[i] = new Vector2(screenPos.x / Screen.width, screenPos.y / Screen.height);
+        }
+        mesh.uv = uvs;
+    }
+
+    void AddMeshToCombine(MeshFilter meshFilter)
+    {
+        CombineInstance combine = new CombineInstance();
+        combine.mesh = meshFilter.mesh;
+        combine.transform = meshFilter.transform.localToWorldMatrix;
+        combineInstances.Add(combine);
+    }
+
+    void SaveIndividualMesh(MeshFilter meshFilter)
+    {
+        if (saveMeshes)
+        {
+            Mesh clone = new Mesh();
+            clone.name = meshFilter.name;
+            clone.vertices = meshFilter.mesh.vertices;
+            clone.triangles = meshFilter.mesh.triangles;
+            clone.normals = meshFilter.mesh.normals;
+            clone.uv = meshFilter.mesh.uv;
+
+            string individualPath = $"{path}/{clone.name}_{Time.time}.asset";
+            #if UNITY_EDITOR
+            AssetDatabase.CreateAsset(clone, individualPath);
+            Debug.Log($"Saved individual mesh at: {individualPath}");
+            AssetDatabase.SaveAssets();
+            #endif
+        }
+    }
+
+    void CombineAllMeshes()
+    {
+        Mesh finalMesh = new Mesh();
+        finalMesh.CombineMeshes(combineInstances.ToArray(), true, true);
+        finalMesh.RecalculateBounds();
+
+        #if UNITY_EDITOR
+        if (saveMeshes)
+        {
+            string finalPath = $"{path}/{name}.asset";
+            AssetDatabase.CreateAsset(finalMesh, finalPath);
+            Debug.Log($"Saved combined mesh at: {finalPath}");
+            AssetDatabase.SaveAssets();
+        }
+        #endif
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(target.position);
+            Debug.Log("target is " + screenPos.x / Screen.width + " pixels from the left");
+        }
+    }
+}
